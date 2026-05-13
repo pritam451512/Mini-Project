c71e05a9eeeb8096e966344ede6898e67dc9de9b
Pritam Raghuwanshi <rpreetam550@gmail.com>
2026-05-13 14:02:39 +0530

    fix auth middleware

--- Backend/middleware/userAuth.js
+++ Backend/middleware/userAuth.js
@@ -1,30 +1,40 @@
- const jwt = require("jsonwebtoken");
- const redisClient = require("../config/redis");
+const jwt = require("jsonwebtoken");
+const redisClient = require("../config/redis");
 const User = require("../Models/users");
 
 const auth = async(req,res,next)=>{
 try{
 
-const {token} = req.cookies;
+const authHeader =
+req.headers.authorization;
 
-if(!token){
+if(!authHeader){
 throw new Error("Please login first")
 }
 
-// // verify token
-// const decoded = jwt.verify(token,process.env.JWT_SECRET_KEY);
+const token =
+authHeader.split(" ")[1];
 
- 
 // check blocked token
-const isBlocked = await redisClient.exists(`token:${token}`);
-if(isBlocked) throw new Error("Invalid Token");
+const isBlocked =
+await redisClient.exists(`token:${token}`);
+
+if(isBlocked)
+throw new Error("Invalid Token");
 
 // verify token
-const decoded = jwt.verify(token,process.env.JWT_SECRET_KEY);
+const decoded =
+jwt.verify(
+token,
+process.env.JWT_SECRET_KEY
+);
 
 // check user exist
-const user = await User.findById(decoded._id);
-if(!user) throw new Error("User not found");
+const user =
+await User.findById(decoded._id);
+
+if(!user)
+throw new Error("User not found");
 
 // user info attach
 req.user = user;
@@ -32,7 +42,9 @@ req.user = user;
 next();
 
 }catch(err){
-res.status(401).send("Unauthorized: " + err.message)
+res.status(401).send(
+"Unauthorized: " + err.message
+)
 }
 }
 
